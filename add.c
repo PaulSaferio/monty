e965a7dcc5f0670c58f1666950137c7cad3060af
Paul Saferio <32652882+PaulSaferio@users.noreply.github.com>
2022-10-05 17:38:48 +0300

    adding errors!

--- add.c
+++ add.c
@@ -1,53 +1,41 @@
-#include <stdio.h>
-
-#include <ctype.h>
-
-#include <stdlib.h>
-
-#include <string.h>
-
 #include "monty.h"
-
-
-
 /**
-
- * _add -  adds the first two nodes of the stack
-
- * @stack: stack given by main
-
- * @line_cnt: line counter
-
- *
-
- * Return: void
-
+ * add - adds the top two elements of the stack
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
  */
-
-void _add(stack_t **stack, unsigned int line_cnt)
-  
+void add(stack_t **stack, unsigned int line_number)
 {
-  
-  int result;
-  
+	stack_t *temp = NULL;
+	stack_t *temp2 = NULL;
+	int i = 0, j = 0, flag = 0;
 
-  
-  if (!stack || !*stack || !((*stack)->next))
-    
-    {
-      
-      fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
-      
-      exit(EXIT_FAILURE);
-      
-    }
-  
+	if (*stack == NULL || stack == NULL)
+	{
+		op_e(line_number, "add");
+	}
+	temp = *stack;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		i++;
+		flag = 1;
+	}
+	temp2 = *stack;
+	while (j < (i - 1))
+	{
+		temp2 = temp2->next;
+		j++;
+	}
+	if (i == 0 && flag == 0)
+		free_dlistint(*stack), op_e(line_number, "add");
+	else
+	{
+		temp2->n = temp2->n + temp->n;
+		temp2->next = NULL;
+		free(temp);
+		temp = NULL;
+		return;
+	}
 
-  
-  result = ((*stack)->next->n) + ((*stack)->n);
-  
-  pop(stack, line_cnt); /*For top node*/
-  
-  (*stack)->n = result;
-  
 }
